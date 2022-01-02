dafb5aa8116025394dd765e67dc7e37cb12179b3
John Asaad <johnnagy91@yahoo.com>
2022-01-02 22:38:51 +0200

    login and sign up edited

--- lib/login_screen.dart
+++ lib/login_screen.dart
@@ -12,148 +12,150 @@ class LoginScreen extends StatelessWidget {
       backgroundColor: Colors.white,
       body: Container(
         child: SingleChildScrollView(
-          child: Column(
-            children: [
-              //container for all screen with background image
-              Container(
-                height: 800,
-                decoration: BoxDecoration(
-                  image: DecorationImage(
-                    image: AssetImage('assets/images/BG.png'),
-                    fit: BoxFit.cover
-                  )
-                ),
-                child: Stack(
-                  //stack for login text and two text fields and a button
-                  children: [
-                    Positioned(
-                      child: Container(
-                        //For Login Text
-                        margin: EdgeInsetsDirectional.only(bottom: 300),
-                        child: Center(
-                          child: Text(
-                            "Log\nIn",
-                            style: TextStyle(color:Colors.white, fontSize: 95,fontWeight: FontWeight.bold,),
-                          )
+          child: Expanded(
+            child: Column(
+              children: [
+                //container for all screen with background image
+                Container(
+                  height: 800,
+                  decoration: BoxDecoration(
+                    image: DecorationImage(
+                      image: AssetImage('assets/images/BG.png'),
+                      fit: BoxFit.cover
+                    )
+                  ),
+                  child: Stack(
+                    //stack for login text and two text fields and a button
+                    children: [
+                      Positioned(
+                        child: Container(
+                          //For Login Text
+                          margin: EdgeInsetsDirectional.only(bottom: 300),
+                          child: Center(
+                            child: Text(
+                              "Log\nIn",
+                              style: TextStyle(color:Colors.white, fontSize: 95,fontWeight: FontWeight.bold,),
+                            )
+                          ),
                         ),
                       ),
-                    ),
-                    Container(
-                      //contains text fields
-                      padding: EdgeInsetsDirectional.only(top:400),
-                      child: Padding(
-                        padding:  EdgeInsets.all(30.0),
-                        child: Column(
-                          children: [
-                            Container(
-                              padding: EdgeInsets.all(5.0),
-                              decoration: BoxDecoration(
-                                  color: Colors.white,
-                                  borderRadius: BorderRadius.circular(15),
-                                  boxShadow: [
-                                    BoxShadow(
-                                      color: Color.fromRGBO(77 , 77, 77, .4),
-                                      blurRadius: 20.0,
-                                      offset: Offset(0 , 10)
+                      Container(
+                        //contains text fields
+                        padding: EdgeInsetsDirectional.only(top:400),
+                        child: Padding(
+                          padding:  EdgeInsets.all(30.0),
+                          child: Column(
+                            children: [
+                              Container(
+                                padding: EdgeInsets.all(5.0),
+                                decoration: BoxDecoration(
+                                    color: Colors.white,
+                                    borderRadius: BorderRadius.circular(15),
+                                    boxShadow: [
+                                      BoxShadow(
+                                        color: Color.fromRGBO(77 , 77, 77, .4),
+                                        blurRadius: 20.0,
+                                        offset: Offset(0 , 10)
+                                      ),
+                                    ]
+                                ),
+                                child: Column(
+                                  children: [
+                                    Container(
+                                      padding: EdgeInsets.all(7.0),
+                                      decoration: BoxDecoration(
+                                          border: Border(bottom: BorderSide(color: Colors.white30))
+                                      ),
+                                      child: TextField(
+                                        controller: emailController,
+                                        keyboardType: TextInputType.emailAddress,
+                                        decoration: InputDecoration(
+                                            border: InputBorder.none,
+                                            labelText: "Email",
+                                            labelStyle: TextStyle(color: Colors.grey[400]) ,
+                                            prefixIcon: Icon(
+                                              Icons.email_outlined,
+                                            ),
+                                            prefixIconColor: Colors.grey[400],
+                                        ),
+                                      ),
+
                                     ),
-                                  ]
-                              ),
-                              child: Column(
-                                children: [
-                                  Container(
-                                    padding: EdgeInsets.all(7.0),
-                                    decoration: BoxDecoration(
-                                        border: Border(bottom: BorderSide(color: Colors.white30))
-                                    ),
-                                    child: TextField(
-                                      controller: emailController,
-                                      keyboardType: TextInputType.emailAddress,
-                                      decoration: InputDecoration(
-                                          border: InputBorder.none,
-                                          labelText: "Email",
+                                    Container(
+                                      padding: EdgeInsets.all(7.0),
+
+                                      child: TextField(
+                                        controller: passwordController,
+                                        keyboardType: TextInputType.visiblePassword,
+                                        obscureText: true,
+                                        decoration: InputDecoration(
+                                            border: InputBorder.none,
+                                          labelText: "Password",
                                           labelStyle: TextStyle(color: Colors.grey[400]) ,
                                           prefixIcon: Icon(
-                                            Icons.email_outlined,
+                                            Icons.lock_outline,
                                           ),
-                                          prefixIconColor: Colors.grey[400],
-                                      ),
-                                    ),
-
-                                  ),
-                                  Container(
-                                    padding: EdgeInsets.all(7.0),
-
-                                    child: TextField(
-                                      controller: passwordController,
-                                      keyboardType: TextInputType.visiblePassword,
-                                      obscureText: true,
-                                      decoration: InputDecoration(
-                                          border: InputBorder.none,
-                                        labelText: "Password",
-                                        labelStyle: TextStyle(color: Colors.grey[400]) ,
-                                        prefixIcon: Icon(
-                                          Icons.lock_outline,
-                                        ),
-                                        suffixIcon: Icon(
-                                          Icons.remove_red_eye_outlined,
+                                          suffixIcon: Icon(
+                                            Icons.remove_red_eye_outlined,
+                                          ),
                                         ),
                                       ),
+
                                     ),
 
-                                  ),
-
-                                ],
-                              ),
-                            ),
-                            SizedBox(height: 30,),
-                            Container(
-
-                              //for login button
-
-                              height: 50,
-                              decoration: BoxDecoration(
-                                  borderRadius: BorderRadius.circular(15),
-                                  gradient: LinearGradient(
-                                    colors: [
-                                      Color.fromRGBO(51 , 51, 51, 1),
-                                      Color.fromRGBO(60 , 60, 60, 1),
-
-                                    ]
-                                  )
-                              ),
-                              child: Center(
-                                child: MaterialButton(
-                                  onPressed: ()
-                                  {
-                                    print(emailController.text);
-                                    print(passwordController.text);
-                                  },
-                                  child: Text('Login',style: TextStyle(color: Colors.white,fontWeight: FontWeight.bold),),
+                                  ],
                                 ),
                               ),
-                            ),
-                            SizedBox(height: 40,),
-                            Row(
+                              SizedBox(height: 30,),
+                              Container(
 
-                              //for register text
-                              mainAxisAlignment: MainAxisAlignment.center,
-                              children: [
-                                Text('Don\'t have an account?', style: TextStyle(color: Colors.white,),),
-                                TextButton(
-                                    onPressed: (){},
-                                     child: Text('Register'),
-                                )
-                              ],
-                            ),
-                          ],
+                                //for login button
+
+                                height: 50,
+                                decoration: BoxDecoration(
+                                    borderRadius: BorderRadius.circular(15),
+                                    gradient: LinearGradient(
+                                      colors: [
+                                        Color.fromRGBO(51 , 51, 51, 1),
+                                        Color.fromRGBO(60 , 60, 60, 1),
+
+                                      ]
+                                    )
+                                ),
+                                child: Center(
+                                  child: MaterialButton(
+                                    onPressed: ()
+                                    {
+                                      print(emailController.text);
+                                      print(passwordController.text);
+                                    },
+                                    child: Text('Login',style: TextStyle(color: Colors.white,fontWeight: FontWeight.bold),),
+                                  ),
+                                ),
+                              ),
+                              SizedBox(height: 40,),
+                              Row(
+
+                                //for register text
+                                mainAxisAlignment: MainAxisAlignment.center,
+                                children: [
+                                  Text('Don\'t have an account?', style: TextStyle(color: Colors.white,),),
+                                  TextButton(
+                                      onPressed: (){},
+                                       child: Text('Register'),
+                                  )
+                                ],
+                              ),
+                            ],
+                          ),
                         ),
                       ),
-                    ),
-                  ],
+                    ],
+                  ),
                 ),
-              ),
 
-            ],
+              ],
+            ),
           ),
         ),
       ),
